d6d56155da20410e15f138347f0a20ee5da39fcd
silazemli <yadefya@gmail.com>
2024-11-09 13:48:39 +0300

    did some things

--- internal/services/loyalty/server.go
+++ internal/services/loyalty/server.go
@@ -1,1 +1,46 @@
 package loyalty
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type server struct {
+	srv echo.Echo
+	db loyaltyStorage
+}
+
+func NewServer(db loyaltyStorage) server {
+	srv := server{}
+	srv.db = db
+	srv.srv = *echo.New()
+	api := srv.srv.Group("/api/loyalty")
+	api.GET("/username", srv.GetStatus)
+	api.PATCH("/increment/username", srv.IncrementCounter)
+	api.PATCH("/decrement/username", srv.DecrementCounter)
+}
+
+func (srv *server) Start() error {
+	err := srv.srv.Start(":8050")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (srv *server) GetStatus(ctx echo.Context) error {
+	status, err := srv.db.GetStatus(ctx.Param("username"))
+	if errors.Is(err, )
+}
+
+func (srv *server) IncrementCounter(ctx echo.Context) error {
+
+}
+
+func (srv *server) DecrementCounter(ctx echo.Context) error {
+
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,4 +1,4 @@
-package loyalty
+package storage
 
 import (
 	"os"
